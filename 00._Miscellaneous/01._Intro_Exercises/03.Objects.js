40b1fb45ceef31d75846adbc6fff8cb8c575cfe5
Tomas Martins <toma3026@stud.kea.dk>
2023-08-27 20:25:47 +0200

    node

--- 00._Miscellaneous/01._Intro_Exercises/03.Objects.js
+++ 00._Miscellaneous/01._Intro_Exercises/03.Objects.js
@@ -7,11 +7,16 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+const ex1res1 = myObj.message;
+console.log(ex1res1);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const ex2res1 = {name: "Tomas Martins", age: "31"};
+console.log(ex2res1);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -20,6 +25,9 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+console.log(stackOverflow);
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,6 +35,9 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
+console.log(thisSong);
 
 // --------------------------------------
 
--- 00._Miscellaneous/01._Intro_Exercises/04.Arrays.js
+++ 00._Miscellaneous/01._Intro_Exercises/04.Arrays.js
@@ -6,6 +6,8 @@
 const letters = ["a", "b", "c"];
 // show b in the console 
 
+const ex1res1 = letters[1];
+console.log(ex1res1);
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -14,6 +16,10 @@ const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
+friends.push("Aragorn");
+friends.push("Legolas");
+friends.push("Gimli");
+console.log(friends);
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -23,6 +29,10 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
 
+const ex3res1 = significantMathNumbers.indexOf(1729);
+console.log(ex3res1);
+const ex3res2 = significantMathNumbers[ex3res1];
+console.log(ex3res2);
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -31,7 +41,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
-
+diet.splice(2, 0, "hamburger", "soda", "pizza");
+console.log(diet);
 
 
 // --------------------------------------
@@ -41,7 +52,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
 
-
+diet.pop();
+console.log(diet);
 
 
 // --------------------------------------
@@ -49,7 +61,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+ex6res1 = structuredClone(diet);
+console.log(ex6res1);
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -58,7 +71,10 @@ const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
 
-
+for (let i = 1; i < lettersExpanded.length; i = i + 2) {
+    const c = lettersExpanded[i];
+    console.log(c);
+}
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
@@ -69,6 +85,18 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 6 || numbers[i] < 0) {
+        const n = numbers[i];
+        console.log(n);
+    }
+    else {
+        discardedNumbers.push(numbers[i]);
+    }
+}
+
+console.log(discardedNumbers);
 
 // --------------------------------------
 
